d2a66f2aafcaff839d450b8955e1540f209ff6b0
kimyurim <soo054141@gmail.com>
2022-04-07 22:32:21 +0900

    Create Redux store

--- src/index.js
+++ src/index.js
@@ -2,12 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { GlobalStyle } from "./styles/GlobalStyle";
 import App from "./App";
+import store from "./store";
+import { Provider } from "react-redux";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <GlobalStyle />
     <App />
-  </React.StrictMode>
+  </Provider>,
 );
